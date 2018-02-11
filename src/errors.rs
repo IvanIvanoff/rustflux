4e5365e4d062074aaff031f102d97116fd680343
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-02-11 22:31:56 +0200

    http client get and post now return RustfluxError

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,29 @@
-#[derive(Debug)]
+use std::fmt;
+
 pub enum RustfluxError {
     InputParse(String),
     JsonDecode(String),
     GetRequest(String),
     PostRequest(String),
+}
+
+impl fmt::Display for RustfluxError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RustfluxError::InputParse(ref error) => write!(f, "Error parsing input: {}", error),
+            RustfluxError::JsonDecode(ref error) => write!(f, "Error decoding json: {}", error),
+            RustfluxError::GetRequest(ref error) => {
+                write!(f, "Error sending GET request: {}", error)
+            }
+            RustfluxError::PostRequest(ref error) => {
+                write!(f, "Error sending POST request: {}", error)
+            }
+        }
+    }
+}
+
+impl fmt::Debug for RustfluxError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
 }
--- src/http_client/mod.rs
+++ src/http_client/mod.rs
@@ -1,15 +1,39 @@
 extern crate reqwest;
+use errors::RustfluxError;
 
 ///
-pub fn get(url: &str) -> Result<String, reqwest::Error> {
-    let mut resp = reqwest::get(url)?;
-    let body = resp.text()?;
-    Ok(body)
+pub fn get(url: &str) -> Result<String, RustfluxError> {
+    match get_internal(url) {
+        Ok(body) => Ok(body),
+        Err(_) => Err(RustfluxError::GetRequest(String::from(
+            "Error executing GET request",
+        ))),
+    }
+}
+
+pub fn post(url: &str) -> Result<String, RustfluxError> {
+    match post_internal(url) {
+        Ok(body) => Ok(body),
+        Err(_) => Err(RustfluxError::GetRequest(String::from(
+            "Error executing GET request",
+        ))),
+    }
 }
 
 ///
-pub fn post(url: &str) -> Result<String, reqwest::Error> {
+fn post_internal(url: &str) -> Result<String, reqwest::Error> {
     let client = reqwest::Client::new();
-    let _body = client.post(url).send()?;
-    Ok(String::from("sent"))
+    let response = client.post(url).send()?;
+
+    match response.status().is_success() {
+        true => Ok(String::from("Success")),
+        false => Ok(String::from("Request not successful    ")),
+    }
+}
+
+// Helper functions
+fn get_internal(url: &str) -> Result<String, reqwest::Error> {
+    let mut resp = reqwest::get(url)?;
+    let body = resp.text()?;
+    Ok(body)
 }
